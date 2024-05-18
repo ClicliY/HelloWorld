e0f277157f9478b55b4d47bb5207f3acb6e1fd32
ClicliY <1822819000@qq.com>
2024-05-18 10:23:49 +0800

    add .dat file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
-	return 0;
+    FILE *fp = NULL;
+
+    fp = fopen("test.dat", "wb");
+    if(NULL != fp)
+    {
+        fputs(fp, "hello world\n");
+        fclose(fp);
+    }
+    else
+    {
+        printf("file open failed\n");
+    }
+    printf("hello world\n");
+    return 0;
 }
